4b9fafadc70a2197b82723b72556135b3ff5903f
Andrei Krotov <akrotoff95@gmail.com>
2023-05-07 18:47:10 +0300

    Initial Commit

---
+++ Tests/ConstraintsKitTests/ConstraintsKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ConstraintsKit
+
+final class ConstraintsKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ConstraintsKit().text, "Hello, World!")
+    }
+}
